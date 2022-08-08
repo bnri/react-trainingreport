6c44822f65e54c837e566c8c60513a8fc97cf9d1
HelloJeong <jeongyk92@gmail.com>
2022-08-08 14:42:20 +0900

    type 작업중

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,86 @@
+import { useMemo } from "react";
+import { Doughnut } from "react-chartjs-2";
+import "chartjs-plugin-doughnutlabel";
+
 import "./App.css";
 
 const App: React.FC = () => {
-  return <div className="App">Hello?</div>;
+  const dummyData = useMemo(() => {
+    return {
+      labels: [],
+      type: "doughnut",
+      datasets: [
+        { data: [34, 66], backgroundColor: ["blue", "transparent"] },
+        { data: [66, 34], backgroundColor: ["red", "transparent"] },
+      ],
+    };
+  }, []);
+
+  const dummyOptions = useMemo(() => {
+    return {
+      title: {
+        display: true,
+        text: "Reading 진단현황 (2개월)",
+        fontSize: 14,
+        fontFamily: "'FontAwesome','Helvetica Neue', 'Helvetica', 'Arial', sans-serif", //
+      },
+      cutoutPercentage: 75,
+      responsive: true,
+      responsiveAnimationDuration: 1200,
+      animation: {
+        duration: 1200,
+      },
+      maintainAspectRatio: false,
+      plugins: {
+        doughnutlabel: {
+          labels: [
+            {
+              text: `50점`,
+              font: {
+                size: "24",
+              },
+              color: "black",
+            },
+            {
+              text: `그룹평균`,
+              font: {
+                size: "16",
+              },
+              color: "#333",
+            },
+            {
+              text: `20점`,
+              font: {
+                size: "18",
+              },
+              color: "red",
+            },
+          ],
+        },
+        datalabels: {
+          display: false,
+          color: "black",
+          font: {
+            size: 14,
+            weight: "bold",
+          },
+        },
+      },
+    };
+  }, []);
+
+  return (
+    <div className="App">
+      <div
+        style={{
+          width: "50%",
+          height: "50%",
+        }}
+      >
+        <Doughnut data={dummyData} options={dummyOptions} />
+      </div>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/types.d.ts
@@ -0,0 +1,54 @@
+type typenames =
+  | "SentenceMask"
+  | "CategoryFinding"
+  | "KeywordFinding"
+  | "WordOrdering"
+  | "VisualSpan"
+  | "VisualCounting"
+  | "TMT"
+  | "Stroop"
+  | "SaccadeTracking"
+  | "PursuitTracking"
+  | "AntiTracking"
+  | "SentenceTracking"
+  | "ExerciseHorizontal"
+  | "ExerciseVertical"
+  | "ExerciseHJump";
+
+interface TrainingType {
+  type: typenames;
+  level: number;
+  language?: "한국어" | "영어";
+  reculsiveCount: number; // 일 수행횟수(recul)
+  weeklyPerformedDays: number; // 주당 수행일(dayofweek 개수)
+  totPerformedCount: number; // 총 수행횟수
+  performedRatio: number; // 수행률
+  totDuration: number; // 총 수행시간
+  avgScore: number; // 평균점수
+  totScore: number; // 총 획득 점수
+}
+
+interface ReportType {
+  agencyID: string;
+  agencyName: string;
+  testeeID: string;
+  testeeNickname: string;
+  startdate: string; // 기준 시작일
+  enddate: string; // 기준 종료일
+  quarterScore: number; // 분기 누적점수(리포트 버튼을 누르는 그 순간의 점수)
+  quarterRank: number; // 분기 누적순위(학원내에서)
+  monthScore: number; // 기간 내의 점수
+
+  performedRatio: number; // 전체 수행률
+  avgScore: number; // 평균 수행 점수
+  avgDuration: number; // 일 평균 수행 시간
+
+  trainingList: TrainingType[];
+  groupScoreList: {
+    performedRatio: number;
+    avgScore: number;
+    avgDuration: number;
+
+    [key: typenames]: number;
+  };
+}
